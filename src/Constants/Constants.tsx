c9e8775f509b0a8ef769c8013844cc91e0d6064e
Matt Klas <matthewklas@gmail.com>
2021-02-17 07:32:52 -0600

    Update Constants.tsx

--- src/Constants/Constants.tsx
+++ src/Constants/Constants.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 
-export const authEndpoint = "https://accounts.spotify.com/authorize";
+export const authEndpoint = "https://accounts.spotify.com/authorize"
 
 // Replace with your app's client ID, redirect URI and desired scopes
-export const clientId = "a7d9f2be243d45f69ad9e83e2ef03b61";
-export const redirectUri = "http://localhost:3000/spotify-playlist-plus";
+export const clientId = "a7d9f2be243d45f69ad9e83e2ef03b61"
+export const redirectUri = "http://localhost:3000/spotify-playlist-plus"
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
