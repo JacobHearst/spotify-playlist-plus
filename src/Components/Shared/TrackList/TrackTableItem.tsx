ae4d00ff249cea8d48f23c34c924196c596f2cbf
TylerWanta <fuzzy1170@gmail.com>
2021-02-21 14:52:09 -0600

    Updated Artist Page. Set up axios. Created player support

--- src/Components/Shared/TrackList/TrackTableItem.tsx
+++ src/Components/Shared/TrackList/TrackTableItem.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Track } from "../../../Models/Tracks"
+import PlayerButton from "../PlayerButton"
 
 interface TrackTableItemProps {
     track: Track
@@ -16,6 +17,7 @@ export default class TrackTableItem extends React.Component<TrackTableItemProps,
     render() {
         return (
             <tr>
+                <PlayerButton currentlyPlaying={false}>&#21E8</PlayerButton>
                 <td>{this.state.track.track_number}</td>
                 <td>{this.state.track.name}</td>
                 <td>{this.state.track.album?.name ?? "No album"}</td>
---
+++ src/Constants/Symbols.ts
@@ -0,0 +1,6 @@
+export enum TrackSymbols {
+    Play = "#9654",
+    Pause = "#9208",
+    Next = "#5861",
+    Previous = "#5862"
+}
